6c2b789b124d9d34f037bd753b58ca523833d1a5
Bruno Araujo <brunoaraujo2395@gmail.com>
2023-03-25 19:26:55 -0300

    finalizando projeto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 export const Header = () => {
 
     const [position, setPosition] = useState(window.pageYOffset);
+    
     const navigate = useNavigate();
 
     let path = window.location.pathname;
