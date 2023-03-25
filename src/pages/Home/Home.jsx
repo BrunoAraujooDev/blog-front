919e0cb560fe87ada79afc15bc71aa9671c893fb
Bruno Araujo <brunoaraujo2395@gmail.com>
2023-03-25 20:02:36 -0300

    colocando navegacao no banner principal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import LayoutCard from '../../components/LayoutCards/LayoutCard'
 import { BannerSection, HighlightedSection, HomeMain, MainTopicDiv, TextButton, TextDiv, TextImg, TextParagraph, TextTitle } from './style'
 
 export const Home = () => {
 
-  const postRandom = {
-    title: 'Morbi lorem dolor, ultricies ut finibus ut.',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et eros elit. Donec sit amet lobortis massa. Sed et ullamcorper massa. Nam accumsan, libero non facilisis posuere, mauris sapien bibendum diam, vitae bibendum velit risus vitae nisl. ',
-  }
+  const navigate = useNavigate();
 
   return (
     <HomeMain>
@@ -26,7 +24,7 @@ export const Home = () => {
               Vivamus et eros elit. Donec sit amet lobortis massa. 
               Sed et ullamcorper massa. Nam accumsan, libero non facilisis posuere, mauris sapien bibendum diam, vitae bibendum velit risus vitae nisl. 
             </TextParagraph>
-            <TextButton>Ver todos</TextButton>
+            <TextButton onClick={() => navigate('/posts/')}>Ver todos</TextButton>
 
           </TextDiv>
           <TextImg src="https://c4.wallpaperflare.com/wallpaper/428/376/859/brazil-christ-cristo-hills-wallpaper-preview.jpg" alt="Imagem do Cristo Redentor no fim da tarde" />
