28f568cdad9e779705c2eb67c49d701135b82997
Bruno Araujo <brunoaraujo2395@gmail.com>
2023-03-25 20:47:08 -0300

    ajuste final na formula

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,9 +1,13 @@
 
 
 const generateRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    const random = Math.random();
+
+    const difference = max - min;
+
+    const randomInsideRange = (Math.round(difference * random) + min);
+
+    return randomInsideRange
 }
 
 
